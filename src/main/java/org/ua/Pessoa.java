ebe0672918a372e09c6a4244e622b2d666c282b6
Renanmontanopaz <renanmontanopaz@outlook.com>
2023-04-12 20:51:44 -0300

    classe pessoa

---
+++ src/main/java/org/ua/Pessoa.java
@@ -0,0 +1,36 @@
+package org.ua;
+
+/**
+ * Criar uma classe PESSOA e adicionar os seguintes atributos para ela
+ * - Nome
+ * - Idade
+ * - Endereço
+ * - Gênero
+ * - Telefone
+ * - Tempo de trabalho
+ *
+ * Essa classe deve ter um contrutor publico para instanciar todos os atributos listados acima
+ *
+ * Os métodos são:
+ * -> getNome> Retorna o nome
+ * -> getIdade> Retorna a idade
+ * -> getEndereço> Retorna o endereço
+ * -> getDadosCompletos> Retorna concatenado NOME, IDADE, GENERO, TELEFONE e TEMPO DE TRABALHO
+ * -> getNomeETelefone> Retorna nome e telefone.
+ * -> getTempoDeTrabalho> Retorna o tempo de trabalho.
+ * -> getTempoParaSeAposentar> Assuma que a idade de aposentadoria é 60 anos feminino, 65 masculino.
+ * -> getTelefone> Retorna o telefone.
+ *
+ * Em seguida crie uma classe de teste para PESSOA e cubra as funcionalidades
+ * com testes unitários para validar o correto funcionamento da mesma.
+ *
+ *
+ * ENTREGA
+ *
+ * A partir da branch aula-02 (upstram), deve ser criada uma branch com o seguinte padrão: ua-nome-sobrenome
+ * e nela realizar o desenvolvimento da atividade. Após concluída, a ativade, gerar um PR (pull request) apontando para a branch
+ * entrega-02 lá do repositório do professor.
+ */
+
+public class Pessoa {
+}
